43a6de83e707228bc9d0fc6f6b73a64a9c282402
Nakajima Kazuki <nakajima.k0301@gmail.com>
2019-10-31 12:07:17 +0900

    update

--- obs_notification/mail.py
+++ obs_notification/mail.py
@@ -1,2 +1,24 @@
+#!/usr/bin/env python
 """mail.py
 """
+
+from smtplib import SMTP_SSL
+from logzero import logger
+
+
+class Email():
+    def __init__(self):
+        self.body = ''
+
+    def send(self):
+        # TODO: do something
+        logger.info('send')
+        pass
+
+    def noop(self):
+        with SMTP_SSL("smtp.lolipop.jp", 465) as smtps:
+            return smtps.noop()
+
+
+if __name__ == '__main__':
+    mail_object = Email()
